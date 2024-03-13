cba54c9453c7bb40e251d199a8bb8fdd8c247bed
Nishanur Rahman <nishan.rahman@yahoo.com>
2024-03-14 03:08:09 +0530

    hi

--- GPS - Track the route.py
+++ GPS - Track the route.py
@@ -1,3 +1,4 @@
+"""
 import os
 import csv
 from gmplot import gmplot
@@ -21,3 +22,27 @@ with open('route.csv', 'r') as file:
 gmap.marker(lat, lon, 'red')
 gmap.draw("mymap.html")
 
+"""
+import csv
+from gmplot import gmplot
+
+gmap = gmplot.GoogleMapPlotter(28.689169, 77.324448, 17)
+
+gmap.coloricon = "http://www.googlemapsmarkers.com/v1/%s/"
+
+with open('route.csv', 'r') as f:
+    reader = csv.reader(f)
+    k = 0
+    
+    for row in reader:
+        lat = float(row[0])
+        long = float(row[1])
+        if(k == 0):
+            gmap.marker(lat,long, 'yellow')
+            k = 1
+        else:
+            gmap.marker(lat,long, 'blue')
+            
+gmap.marker(lat,long, 'red')
+
+gmap.draw("mymap.html")
